c06a72c7f0617b8cb340d9923d15dcc865c8a449
david elliott <taboularasa@hope.dreamhost.com>
2009-04-16 10:41:43 -0700

    updated footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 
 	<div id="footer" class="span-18 last">
-		<div id="footer-nav" class="span-11 prepend-7 last">
+		<div id="footer-nav" class="span-11 prepend-9 last">
 			<span class="footer-link"><a title="PROJECTS" href="http://hadto.net/category/projects/">PROJECTS</a></span>
 			<span class="meta-sep">|</span>
 			<span class="footer-link"><a title="MOVIES" href="http://hadto.net/category/movies/">MOVIES</a></span>
 			<span class="meta-sep">|</span>
-			<span class="footer-link"><a title="PICTURES" href="http://hadto.net/category/pictures/">PICTURES</a></span>
-			<span class="meta-sep">|</span>
+			<!-- <span class="footer-link"><a title="PICTURES" href="http://hadto.net/category/pictures/">PICTURES</a></span>
+						<span class="meta-sep">|</span> -->
 			<span class="footer-link"><a title="WRITING" href="http://hadto.net/category/writing/">WRITING</a></span>
 			<span class="meta-sep">|</span>
 			<span class="footer-link"><a title="SKETCHBOOK" href="http://hadto.net/category/sketchbook/">SKETCHBOOK</a></span>
