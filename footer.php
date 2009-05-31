882c2c90168d5c1fd7d114ce0e9dde71f6552a6d
david elliott <taboularasa@hope.dreamhost.com>
2009-05-30 20:16:14 -0700

    took out pictures section

--- footer.php
+++ footer.php
@@ -16,6 +16,14 @@
 </div><!-- #wrapper .hfeed -->
 
 <?php wp_footer() ?>
-
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8487792-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
 </body>
 </html>
